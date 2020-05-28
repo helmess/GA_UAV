4b3ec6c7e137dc0a48511183323a53298d14cab3
Administrator <Administrator@PC-20200428NASH.mshome.net>
2020-05-28 19:36:15 +0800

    完善了std_ga的算法

--- GA.m
+++ GA.m
@@ -15,13 +15,13 @@ for i=1: (numel(m_x)-1)
    startp=[m_x(miss_seq(i)),m_y(miss_seq(i)),m_z(miss_seq(i))];
    endp =[m_x(miss_seq(i+1)),m_y(miss_seq(i+1)),m_z(miss_seq(i+1))];
    Std_GA(startp,endp,model);
-   Global_Chromosome(i)=GA_Run(startp,endp,model);
-   state_prob=[state_prob,Global_Chromosome(i).sol.MarkovState];
+    Global_Chromosome(i)=GA_Run(startp,endp,model);
+    state_prob=[state_prob,Global_Chromosome(i).sol.MarkovState];
 end
-state_bar =figure(2);
-b=bar(state_prob','stack');
-legend('U','D','T','E','H');
-b(1).FaceColor='w';b(2).FaceColor='y';b(3).FaceColor='m';b(4).FaceColor='r';b(5).FaceColor='k';
+% state_bar =figure(2);
+% b=bar(state_prob','stack');
+% legend('U','D','T','E','H');
+% b(1).FaceColor='w';b(2).FaceColor='y';b(3).FaceColor='m';b(4).FaceColor='r';b(5).FaceColor='k';
 toc;
 
 
--- Std_GA.m
+++ Std_GA.m
@@ -1,32 +1,44 @@
-function [ output_args ] = Std_GA( startp,endp,model )
+function Std_GA( startp,endp,model )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     model.startp=startp;
     model.endp=endp;
+    model.NP=40;
+    model.MaxIt=50;
     %在起始和目的点30等分，获得顺序的x坐标
-    x= linspace(startp(1),endp(1),model.dim);
-    y =linspace(model.Ymin,model.Ymax,30);
-    z=linspace(200,400,25);
+    model.dim=10;
+    x= linspace(startp(1),endp(1),model.dim+2);
+    slice =50;
+    y =linspace(startp(2),endp(2),slice);
+    z=linspace(200,350,slice);
     my_chromosome.pos=[];
     my_chromosome.cost=[];
     my_chromosome.sol=[];
     chromosome = repmat(my_chromosome,model.NP,1);
-     %子代染色体
-    next_chromosome = repmat(my_chromosome,model.NP,1);
     %两代染色体
     AllChromosome = repmat(my_chromosome,model.NP*2,1);
+     %子代染色体
+    next_chromosome = repmat(my_chromosome,model.NP,1);
     %种群的适应度值
     seeds_fitness=zeros(1,model.NP);
     %全局最优
     globel.cost =inf;
-    len =numel(x);
+    len =numel(x)-2;
     %初始化染色体
     for i=1:model.NP
-    for dim=1:len
-        chromosome(i).pos(dim,1)=x(dim);
-        chromosome(i).pos(dim,2)=y(ceil(rand*(len)));
-        chromosome(i).pos(dim,3)=z(ceil(rand*(len)));
-    end
+        if i<2
+            for dim=1:len
+                chromosome(i).pos(dim,1)=x(dim+1);
+                chromosome(i).pos(dim,2)=y(ceil(dim*slice/len));
+                chromosome(i).pos(dim,3)=z(ceil(dim*slice/len));
+            end
+        else
+            for dim=1:len
+            chromosome(i).pos(dim,1)=x(dim+1);
+            chromosome(i).pos(dim,2)=y(ceil(rand*slice));
+            chromosome(i).pos(dim,3)=z(ceil(rand*slice));
+            end
+        end
     [chromosome(i).cost,chromosome(i).sol]=FitnessFunction( chromosome(i),model );
     seeds_fitness(i) = chromosome(i).cost;
     end
@@ -71,12 +83,40 @@ for it=1:model.MaxIt
     sons(2).pos(:,2:3) = sons(2).pos(:,2:3) + k*(ones(model.dim,1)*[model.Ymax,500]-sons(2).pos(:,2:3))*r/10;
     end
     
-    %符合要求以后计算子代的适应度值
+    end
+        %符合要求以后计算子代的适应度值
     [sons(1).cost,sons(1).sol] = FitnessFunction(sons(1),model);
     [sons(2).cost,sons(2).sol] = FitnessFunction(sons(2),model);
     next_chromosome(seed) = (sons(1));
     next_chromosome(seed+1) = (sons(2));
     end
+   %把新旧合并同一种群
+    AllChromosome(1:model.NP) = chromosome(1:model.NP);
+    AllChromosome(model.NP+1:model.NP*2) = next_chromosome(1:model.NP);
+    %精英保留,新旧种群一起比较
+    
+    for i=1:model.NP*2
+    eval_array(i,:) = [i,AllChromosome(i).cost];
+    end
+    %以cost从小到大进行排序
+    eval_array =sortrows(eval_array,2);
+    last_cost=eval_array(1,2);
+    cnt =1;
+    chromosome(cnt) = AllChromosome(eval_array(1,1));
+    %下次迭代的染色体为不重复cost的最优染色体
+    for i=2:model.NP*2
+        current_cost = eval_array(i,2);
+        if current_cost ~= last_cost
+        cnt = cnt+1;
+        chromosome(cnt) = AllChromosome(eval_array(i,1));
+        last_cost = current_cost;
+        end
+    end
+    %如果下次迭代的染色体数目不够，就根据轮盘赌补染色体。
+    cnt_r =cnt;
+    while cnt <model.NP
+        cnt= cnt+1;
+        chromosome(cnt) = AllChromosome(eval_array(cnt - cnt_r,1));
     end
     %选出迭代的染色体和全局最优染色体
     for index =1:model.NP
@@ -85,7 +125,7 @@ for it=1:model.MaxIt
             globel = chromosome(index);
         end
     end
-    
+  
     best(it) = globel.cost;
     disp(['it: ',num2str(it),'   best value:',num2str(globel.cost)]);
     

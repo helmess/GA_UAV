1b12895ae25bc2058b4735eeea83f2e9b769dafb
Administrator <Administrator@PC-20200428NASH.mshome.net>
2020-05-29 15:19:53 +0800

    main函数发现没有添加

--- main.m
+++ main.m
@@ -11,16 +11,16 @@ m_z=[model.sz,model.mission_z,model.ez];
 tic;
 state_prob=[];
 
-for i=1: (numel(m_x)-1)
+for i=3: (numel(m_x)-1)
    startp=[m_x(miss_seq(i)),m_y(miss_seq(i)),m_z(miss_seq(i))];
    endp =[m_x(miss_seq(i+1)),m_y(miss_seq(i+1)),m_z(miss_seq(i+1))];
-   Std_GA(startp,endp,model);
+   %Std_GA(startp,endp,model);
     Global_Chromosome(i)=GA_Run(startp,endp,model);
     state_prob=[state_prob,Global_Chromosome(i).sol.MarkovState];
 end
 % state_bar =figure(2);
 % b=bar(state_prob','stack');
-% legend('U','D','T','E','H');
+% legend('U','D','T','E','H');m_y
 % b(1).FaceColor='w';b(2).FaceColor='y';b(3).FaceColor='m';b(4).FaceColor='r';b(5).FaceColor='k';
 toc;
 
